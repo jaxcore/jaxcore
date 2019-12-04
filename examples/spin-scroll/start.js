615183b1a12a88271f54f28e7b07d1579dccb9ef
Dan Steinman <dansteinman@gmail.com>
2019-12-04 14:54:28 -0500

    add scroll connectAdapter

--- examples/spin-scroll/start.js
+++ examples/spin-scroll/start.js
@@ -5,11 +5,18 @@ jaxcore.addPlugin(require('jaxcore-spin'));
 jaxcore.addPlugin(require('../../plugins/keyboard'));
 jaxcore.addPlugin(require('../../plugins/scroll'));
 
+jaxcore.defineAdapter('scroll-default', {
+	adapterType: 'scroll',
+	deviceType: 'spin',
+	services: {
+		scroll: 'scroll'
+	}
+});
+
 jaxcore.on('spin-connected', function(spin) {
 	console.log('connected', spin.id);
 	
-	// scroll plugin adds the 'scroll' adapter
-	jaxcore.launchAdapter(spin, 'scroll');
+	jaxcore.connectAdapter(spin, 'scroll');
 });
 
 jaxcore.startDevice('spin');
